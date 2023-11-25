90fde09a8a0d1d831ce84516ab19032b7908b544
niktos12 <nikitosss121207@gmail.com>
2023-11-25 21:38:03 +0400

    final?

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,6 @@ const swiper = new Swiper('.swiper', {
     spaceBetween:24,
     slidesPerView: 3,
     direction:'horizontal',
-    // loopedSlides: 1,
-    loopFillGroupWithBlank: true,
     slidesPerGroup: 1,
     loop: true,
     speed:1200,
@@ -11,10 +9,34 @@ const swiper = new Swiper('.swiper', {
     centeredSlides: true,
     mousewheel: true,
     width: 1000,
-    
     autoplay: {
         delay: 3000,
         disableOnInteraction: false,
+    },
+    breakpoints:{
+        1373:{
+            slidesPerView: 2.5,
+            width: 700,
+        },
+        900:{
+            slidesPerView: 1.5,
+            width: 500,
+        },
+        697:{
+            slidesPerView: 1,
+            width: 290,
+            spaceBetween:16,
+        },
+        420:{
+            slidesPerView: 1,
+            width: 250,
+            spaceBetween:16,
+        },
+        364:{
+            slidesPerView: 1,
+            width: 240,
+            spaceBetween:16,
+        }
     },
     navigation:{
         nextEl:'.swiper-navigation-next',
